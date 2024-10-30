044812115748d8c7eae1de291fa38633e6fbbb93
mier002 <micha.erci@students.fhnw.ch>
2024-10-30 21:22:18 +0100

    HA-Funktioniert

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import Typography from '@mui/material/Typography';
+import Quakes from "./Quakes"
 
 function App() {
-  return <Typography variant="h3">Hello React + MUI</Typography>
+  return <Quakes/>
 }
 
 export default App;
